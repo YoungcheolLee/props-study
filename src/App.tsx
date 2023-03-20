5d7267662a32b5d706aa485d31fd49370483d0a1
youngcheolLee <zodiac0905@gmail.com>
2023-03-20 18:38:28 +0900

    App.tsx props study

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+interface Props {
+  num: number;
+  onIncrease?: (number: number) => void;
+}
 
 function App() {
+  const [num, setNum] = useState<number>(10);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Root: {num}</h1>
+        <div className="grid">
+          <Left1 num={num} />
+          <Right1 num={num} onIncrease={(num) => setNum(num + 1)} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Left1(props: Props) {
+  console.log("left1 실행");
+  return (
+    <div>
+      <h1>Left1: {props.num}</h1>
+      <Left2 num={props.num} />
+    </div>
+  );
+}
+
+function Left2(props: Props) {
+  return (
+    <div>
+      <h1>Left2: {props.num}</h1>
+      <Left3 num={props.num} />
+    </div>
+  );
+}
+
+function Left3(props: Props) {
+  return (
+    <div>
+      <h1>Left3:{props.num}</h1>
+    </div>
+  );
+}
+
+function Right1(props: Props) {
+  return (
+    <div>
+      <h1>Right</h1>
+      <Right2 num={props.num} onIncrease={props.onIncrease} />
+    </div>
+  );
+}
+
+function Right2(props: Props) {
+  return (
+    <div>
+      <h1>Right</h1>
+      <Right3 num={props.num} onIncrease={props.onIncrease} />
+    </div>
+  );
+}
+
+function Right3(props: Props) {
+  const handleClickPlusButton = (num: number) => {
+    if (props.onIncrease) {
+      props.onIncrease(num);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Right:{props.num}</h1>
+      <button onClick={() => handleClickPlusButton(props.num)}>+</button>
     </div>
   );
 }
